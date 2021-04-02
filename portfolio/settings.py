3939d6ff51b1ab111fd0d300528ec0d753b33702
khushwant singh <khushwantrathore7865@gmail.com>
2021-04-02 14:34:20 +0530

    adding domain name

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'lc7v@*&=60w!l8d()$+p707b*4(1ofx9c-w#v#-xn5z@-hs3p&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','khushwantsinghrathore.herokuapp.com']
 
 # Application definition
 
